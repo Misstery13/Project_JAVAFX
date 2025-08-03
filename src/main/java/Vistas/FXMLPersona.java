cb4122c6fabeb47bd10a2f7aeff0ce961ec88010
Diana <97330247+Misstery13@users.noreply.github.com>
2025-08-03 02:15:06 -0500

    update fxml telefono

--- src/main/java/Vistas/FXMLPersona.java
+++ src/main/java/Vistas/FXMLPersona.java
@@ -1,9 +1,12 @@
 package Vistas;
 
+import General.BD;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class FXMLPersona
 {
@@ -22,4 +25,27 @@ public class FXMLPersona
 
     @javafx.fxml.FXML
     public void initialize() {
+        //cerrar el formulario actual
+        Stage myStage=(Stage) this.btn_cancelar.getScene().getWindows();
+        myStage()
+    catch (Exception e) {
+
+        }
+        private void acc_grabar(ActionEvent event) {
+            try {
+                if (fun_validar()){
+                    String cedula=this.txt_cedula.getText();
+                    String nombres=this.txt_nombre.getText();
+                    String direccion=this.txt_direccion.getText();
+                    Persona objPersona=new Persona(cedula,nombres,direccion);
+                    BD.personas.add(objPersona);
+                    System.out.println("Se registro con exito");
+                    for (Persona obj : BD.personas) {
+                        System.out.println("CEDULA: "+ obj.getNombres());
+                    }
+
+                }else {
+                    System.out.println("Datos sin validar");
+                }
+            } catch (Exception e) {
     }}
--- src/main/java/Vistas/FXMLTelefono.java
+++ src/main/java/Vistas/FXMLTelefono.java
@@ -1,7 +1,22 @@
 package Vistas;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class FXMLTelefono
 {
+    @javafx.fxml.FXML
+    private TextField txt_num;
+    @javafx.fxml.FXML
+    private TextField txt_ope;
+    @javafx.fxml.FXML
+    private Button btn_salir;
+    @javafx.fxml.FXML
+    private Button btn_guardar;
+    @javafx.fxml.FXML
+    private ComboBox cbo_lista;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
